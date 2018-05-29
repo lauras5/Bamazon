7e4173ebe64e6a4929288f698255886f6800502e
Laura Salcido <idlaurasalcido@gmail.com>
2018-05-29 16:44:16 -0700

    clean up code, add notes

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,10 +1,13 @@
+// this js controls the customer experience
+// require dependencies
 var inq = require('inquirer');
 var mysql = require('mysql');
+
 var ListView = ''
 var userItemNum;
 var userItemAmt;
 
-// Connection
+// Connection to MySQL DB 
 var connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
@@ -17,6 +20,7 @@ connection.connect(function (e) {
   if (e) throw e;
 });
 
+// 1st question
 var q_1 = [
   {
     type: 'input',
@@ -25,6 +29,7 @@ var q_1 = [
   },
 ];
 
+// 2nd question
 var q_2 = [
   {
     type: 'input',
@@ -33,6 +38,7 @@ var q_2 = [
   },
 ];
 
+// throw queries into variables for cleaner code
 var getAns = `
 SELECT * FROM products
 WHERE item_id=?`
@@ -42,53 +48,64 @@ UPDATE products
 SET stock_quantity=?
 WHERE item_id=?`
 
-// var getSales = `
-// SELECT * FROM departments
-// WHERE item_id=?`
-
-// var updateSales = `
-// UPDATE departments
-// SET product_sales=?
-// WHERE item_id=?`
-
 // Connecting and retrieving all products from table
 connection.query('SELECT * FROM products', function (e, r) {
   if (e) throw e;
-  // Loops through products and console logs name, dept, item Info
+
+  // Loops through products array, console logs id, name, dept and price
   for (var i = 0; i < r.length; i++) {
     var itemInfo = r[i]
     console.log(itemInfo.item_id + '. ' + itemInfo.product_name + '\nDepartment : ' + itemInfo.department_name + '\nPrice : $' + itemInfo.price + '\n')
   }
+
+  // call question prompts
   inq.prompt(q_1).then(function (ans) {
     if (e) throw e
+
+    // returns the info for the item they chose
     connection.query(getAns, [ans.itemID], function (e, r) {
-      // console.log(r)
       var itemNum = r[0].item_id
       var cItem = r[0].product_name
       var cItemPrice = r[0].price
       var cItemQty = r[0].stock_quantity
+
+      // logs your selection to confirm choice
       console.log('Your Selection : ' + cItem + ', $' + cItemPrice)
+
+      // second prompt 
       inq.prompt(q_2).then(function (amt) {
         if (e) throw e
+
+        // if the quantity you want is available, then it lets you purchase
         if (amt.amount <= cItemQty) {
-          var newQty = cItemQty - amt.amount
-          var total = amt.amount*cItemPrice
+          var newQty = cItemQty - amt.amountcItemQty - amt.amount
+          var total = amt.amount * cItemPrice
+
           connection.query(updateAmt, [newQty, itemNum], function (e, r) {
             if (e) throw e
+
+            // logs the item you purchased, with the amount and total price
             console.log('You have purchased ' + amt.amount + ' ' + cItem + ', your total is $' + total + '. Thank you for your business!')
-            connection.end(function(e) {
+
+            //close connection
+            connection.end(function (e) {
               if (e) throw e
-            })
-          })
+            });
+          });
+
+        // if there is not enough stock, you get this message
         } else {
           console.log("We don't have enough stock, please come back another time!")
-          connection.end(function(e) {
+
+          // close connection
+          connection.end(function (e) {
             if (e) throw e
-          })
-        }
-      })
-    })
-  })
+          });
+        };
+
+      });
+    });
+  });
 });
 
 
--- bamazonManager.js
+++ bamazonManager.js
@@ -1,8 +1,10 @@
+// this js controls manager experience
+// require dependencies for js
 var inq = require('inquirer');
 var mysql = require('mysql');
 var itemInfo;
 
-// Connection
+// Connection to MySQL DB
 var connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
@@ -42,106 +44,151 @@ var addMore = [
 // New item into MySQL
 var newItems = [
     {
-        type : 'input', 
-        name : 'itemName',
-        message : 'What is the item you would like to add?'
+        type: 'input',
+        name: 'itemName',
+        message: 'What is the item you would like to add?'
     },
     {
-        type : 'input', 
-        name : 'itemPrice',
-        message : 'What price would you like to set for this item? (no symbols)'
+        type: 'input',
+        name: 'itemPrice',
+        message: 'What price would you like to set for this item? (no symbols)'
     },
     {
-        type : 'input', 
-        name : 'itemQuantity',
-        message : 'What is the quantity available for this item?'
+        type: 'input',
+        name: 'itemQuantity',
+        message: 'What is the quantity available for this item?'
     },
     {
-        type : 'input', 
-        name : 'itemDept', 
-        message : 'What is the items department type? (ex : home decor, tech)'
+        type: 'input',
+        name: 'itemDept',
+        message: 'What is the items department type? (ex : home decor, tech)'
     },
 ];
 
-// var updateQty = `
-// UPDATE products
-// SET stock_quantity=?
-// WHERE item_id=?`
+var myQuery = '';
 
-
+// call the dropdown menu
 inq.prompt(listMenu).then(function (ans) {
-    console.log('>>>' + ans.menu[0])
-    var chosenAns = ans.menu[0]
+    // shows log of answer
+    console.log('>>>' + ans.menu[0]);
+    var chosenAns = ans.menu[0];
 
+    // did if else statements, could also do a switch statement here
     if (chosenAns === 'View Products for Sale') {
+
+        // variable for query
+        myQuery = 'SELECT * FROM products';
+
         // id, name, price and qty
-        connection.query('SELECT * FROM products', function (e, r) {
-            if (e) throw e
+        connection.query(myQuery, function (e, r) {
+            if (e) throw e;
+
             for (var i = 0; i < r.length; i++) {
-                itemInfo = r[i]
+
+                itemInfo = r[i];
+
                 console.log(itemInfo.item_id + '. ' + itemInfo.product_name + '\nPrice : $' + itemInfo.price + '\nQuantity : ' + itemInfo.stock_quantity + '\n')
-            }
-            connection.end()
-        })
+            };
+
+            connection.end(function(e) {
+                if (e) throw e;
+            });
+        });
     } else if (chosenAns === 'View Low Inventory') {
-        connection.query('SELECT * FROM products WHERE stock_quantity < 5', function (e, r) {
+
+        // variable for query
+        myQuery = 'SELECT * FROM products WHERE stock_quantity < 5';
+
+        connection.query(myQuery, function (e, r) {
             // If no items are returned
             if (r.length <= 0) {
-                console.log('No items need restocking!')
-                connection.end()
+
+                console.log('No items need restocking!');
+
+                connection.end(function (e) {
+                    if (e) throw e;
+                });
+
             // Otherwise run this to show low inventory items
             } else {
                 for (var i = 0; i < r.length; i++) {
-                    itemInfo = r[i]
+
+                    itemInfo = r[i];
+
                     console.log(itemInfo.item_id + '. ' + itemInfo.product_name + '\nPrice : $' + itemInfo.price + '\nQuantity : ' + itemInfo.stock_quantity + '\n')
-                }
-                connection.end()
-            }
-        })
+
+                };
+
+                connection.end(function(e) {
+                    if (e) throw e;
+                });
+            };
+        });
     } else if (chosenAns === 'Add to Inventory') {
-        connection.query('SELECT * FROM products', function (e, r) {
-            if (e) throw e
+
+        myQuery = 'SELECT * FROM products';
+
+        connection.query(myQuery, function (e, r) {
+            if (e) throw e;
+
             for (var i = 0; i < r.length; i++) {
-                itemInfo = r[i]
-                console.log(itemInfo.item_id + '. ' + itemInfo.product_name + '\nPrice : $' + itemInfo.price + '\nQuantity : ' + itemInfo.stock_quantity + '\n')
-            }
+
+                itemInfo = r[i];
+
+                console.log(itemInfo.item_id + '. ' + itemInfo.product_name + '\nPrice : $' + itemInfo.price + '\nQuantity : ' + itemInfo.stock_quantity + '\n');
+            };
+
             inq.prompt(addMore).then(function (ans) {
-                console.log(ans)
-                var itemToUpdate = ans.itemID
+                console.log(ans);
+
+                var itemToUpdate = ans.itemID;
+
                 // Need to turn into integer because it's a string
-                var stockAdd = parseInt(ans.quantity)
-                connection.query('SELECT * FROM products WHERE item_id=?;', [itemToUpdate], function(e, r) {
-                    itemInfo = r[0]
-                    console.log(itemInfo)
+                var stockAdd = parseInt(ans.quantity);
+
+                connection.query('SELECT * FROM products WHERE item_id=?;', [itemToUpdate], function (e, r) {
+                    itemInfo = r[0];
+                    console.log(itemInfo);
+
                     // Adds current amound and amount entered to get new quantity
-                    var newQty = itemInfo.stock_quantity + stockAdd
+                    var newQty = itemInfo.stock_quantity + stockAdd;
+
                     // Updates in MySQL to new quantities
                     connection.query('UPDATE products SET stock_quantity=? WHERE item_id=?', [newQty, itemToUpdate], function (e, r) {
-                        console.log(r)
-                        if (e) throw e
-                        console.log('Item sucessfully Updated')
-                        connection.end()
-                    })
-                })
-            })
-        })
+                        if (e) throw e;
+                        console.log(r);
+                        console.log('Item sucessfully Updated');
+                        connection.end(function (e) {
+                            if (e) throw e;
+                        });
+                    });
+                });
+            });
+        });
     } else if (chosenAns === 'Add new Product') {
-        inq.prompt(newItems).then(function(ans) {
-            console.log(ans)
-            var iName = ans.itemName
-            var iPrice = parseInt(ans.itemPrice)
-            var iQty = parseInt(ans.itemQuantity)
-            var iDept = ans.itemDept
-            connection.query('INSERT INTO products SET product_name = ?, department_name = ?, price = ?, stock_quantity = ?;', [iName, iDept, iPrice, iQty], function(e, r) {
-                if (e) throw e
-                console.log('You have successfully added a new item!')
-            })
-            connection.end()
-        })
-        // connection.end()
+        inq.prompt(newItems).then(function (ans) {
+            console.log(ans);
+            var iName = ans.itemName;
+            var iPrice = parseInt(ans.itemPrice);
+            var iQty = parseInt(ans.itemQuantity);
+            var iDept = ans.itemDept;
+
+            connection.query('INSERT INTO products SET product_name = ?, department_name = ?, price = ?, stock_quantity = ?;', [iName, iDept, iPrice, iQty], function (e, r) {
+                if (e) throw e;
+
+                console.log('You have successfully added a new item!');
+            });
+
+            connection.end(function (e) {
+                if (e) throw e;
+            });
+        });
     } else {
-        console.log('There has been an error, please try again')
-        connection.end()
-    }
-})
+        console.log('There has been an error, please try again');
+
+        connection.end(function (e) {
+            if (e) throw e;
+        });
+    };
+});
 
